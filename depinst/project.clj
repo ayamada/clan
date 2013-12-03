a0791065e6977c4b7a5ab3dca16202baf90de743
atsuo yamada <yamada-git@tir.jp>
2013-12-04 04:23:21 +0900

    fixes for libgdx.so in libgdx-0.9.9

--- depinst/project.clj
+++ depinst/project.clj
@@ -1,16 +1,5 @@
 ;; this is dummy project to install dependencies for local repos.
 
-(defproject
-  deps-install "0.0.0"
-  ;:description "FIXME: write description"
-  ;:url "http://example.com/FIXME"
-  ;:license {:name "Eclipse Public License"
-  ;          :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies []
-  :source-paths []
-  :java-source-paths []
-  :test-paths []
-  :resource-paths []
-  :plugins [[lein-localrepo "0.5.2"]]
-  )
+(load-file "../../project-common.clj")
+(defproject-depinst)
 
